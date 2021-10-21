b9880cc1e7276029d739b5998d79e64883a47dc7
Evelyn Yarzebinski <eyarzebinski@gmail.com>
2021-10-21 15:29:12 -0400

    updated script examples

--- scripts/script_codebookWorkbook.R
+++ scripts/script_codebookWorkbook.R
@@ -1,33 +1,25 @@
-# CERP Variable Management: Script to generate variable workbook
+# CERP Variable Management: Script to generate codebook workbook
 # EY 2021-10
 
-### First time running this script ----
-# call the library to install the CERP R package
-library(devtools)
+### Run the below code each time you want to generate a new codebook workbook ----
 
-## if the above line of code generates an error (warnings are ok), uncomment and run the below 2 lines. Otherwise, skip.
-# install.packages("devtools")
-# library(devtools)
-
-# install the package
-# You may be prompted to update packages - this is not necessary to run the script.
-# If you are prompted to install any dependency packages, you will need to install those.
-devtools::install("~/Desktop/personal/CERPVariableManagement/")
-
-### Run the below code each time you want to generate a new variable workbook ----
-# fill in each of the variables below
-csvName = "PLMW@ICFP 2019 Feedback_July 1, 2021_14.38.csv"
-savName = "PLMW@ICFP 2019 Feedback_July 1, 2021_14.38.sav"
-rawDataLocation = "~/Downloads/"
-outputFileName = "PLMW@ICFP_2019_variableWorkbook.xlsx"
-outputFileLocation = "~/Desktop/"
-
-# call the package (run this each time you run this script)
+# set up the environment
 library(CERPVariableManagement)
 
+# fill in each of the variables below
+variableWorkbookFilename = "GradCohortIDEALS_2021_pre_variableManagement.xlsx"
+variableWorkbookTabName = "varWorkbook"
+variableWorkbookFileLocation = "[path to variable workbook on Dropbox]"
+dictionaryFilename = "GCIDEALS2021_pre_codebook.csv"
+dictionaryFileLocation = "[path to SPSS dictionary on Dropbox]"
+codebookFilename = "GradCohortIDEALS_2021_codebookWorkbook.xlsx"
+codebookFileLocation = "[path to where to write final codebook file]"
+
 # execute the below call exactly as follows
-CERPVariableManagement::write_variable_workbook(csvName,
-                                                savName,
-                                                rawDataLocation,
-                                                outputFileName,
-                                                outputFileLocation)
+CERPVariableManagement::write_codebook_workbook(variableWorkbookFilename,
+                                                variableWorkbookTabName,
+                                                variableWorkbookFileLocation,
+                                                dictionaryFilename,
+                                                dictionaryFileLocation,
+                                                codebookFilename,
+                                                codebookFileLocation)
--- scripts/script_variableWorkbook.R
+++ scripts/script_variableWorkbook.R
@@ -1,33 +1,21 @@
 # CERP Variable Management: Script to generate variable workbook
 # EY 2021-10
 
-### First time running this script ----
-# call the library to install the CERP R package
-library(devtools)
-
-## if the above line of code generates an error (warnings are ok), uncomment and run the below 2 lines. Otherwise, skip.
-# install.packages("devtools")
-# library(devtools)
-
-# install the package
-# You may be prompted to update packages - this is not necessary to run the script.
-# If you are prompted to install any dependency packages, you will need to install those.
-devtools::install("~/Desktop/personal/CERPVariableManagement/")
-
 ### Run the below code each time you want to generate a new variable workbook ----
+
+# set up the environment
+library(CERPVariableManagement)
+
 # fill in each of the variables below
 csvName = "PLMW@ICFP 2019 Feedback_July 1, 2021_14.38.csv"
 savName = "PLMW@ICFP 2019 Feedback_July 1, 2021_14.38.sav"
 rawDataLocation = "~/Downloads/"
-outputFileName = "PLMW@ICFP_2019_variableWorkbook.xlsx"
-outputFileLocation = "~/Desktop/"
-
-# call the package (run this each time you run this script)
-library(CERPVariableManagement)
+variableWorkbookFilename = "PLMW@ICFP_2019_variableWorkbook_test2.xlsx"
+variableWorkbookFileLocation = "~/Desktop/"
 
 # execute the below call exactly as follows
 CERPVariableManagement::write_variable_workbook(csvName,
                                                 savName,
                                                 rawDataLocation,
-                                                outputFileName,
-                                                outputFileLocation)
+                                                variableWorkbookFilename,
+                                                variableWorkbookFileLocation)
